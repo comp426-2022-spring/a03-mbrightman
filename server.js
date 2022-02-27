d67036f205a9c46752e2210beb014f834c43043a
mbrightman <brightman.matthew@gmail.com>
2022-02-27 11:37:40 -0500

    added endpoints to server.js

--- server.js
+++ server.js
@@ -5,8 +5,13 @@
 const express = require('express')
 const app = express()
 
-// var port = require('minimist')(process.argv.slice(2))
-var port = 5000
+var args = require('minimist')(process.argv.slice(2))
+
+if (args['port'] === undefined) {
+    port = 5000
+} else {
+    port = args['port']
+}
 
 const server = app.listen(port, () => {
     console.log(`App is running on port ${port}`)
@@ -14,7 +19,24 @@ const server = app.listen(port, () => {
 
 function coinFlip() {
     return (Math.floor(Math.random() * 2) == 0) ? 'heads' : 'tails';
-  }
+}
+
+function flipACoin(call) {
+    var flip = (Math.floor(Math.random() * 2) == 0) ? 'heads' : 'tails';
+    if (flip == call) {
+        return {
+        'call': call,
+        'flip': flip,
+        'result': 'win'
+        }
+    } else {
+        return {
+        'call': call,
+        'flip': flip,
+        'result': 'lose'
+        }
+    }
+}
 
 // default endpoint
 app.get('/app', (req,res) => {
@@ -23,13 +45,23 @@ app.get('/app', (req,res) => {
 })
 
 // allows you to go to that endpoint and replace :number with something else
-app.get('/app/echo/:number', (req, res) => {
+app.get('/app/flips/:number', (req, res) => {
     res.status(200).json({ 'message': req.params.number })
 })
 
 app.get('/app/flip', (req, res) => {
     var flip = coinFlip()
     res.status(200).json({ 'flip': flip })
+})
+
+app.get('/app/flip/call/heads', (req, res) => {
+    var flip = flipACoin('heads')
+    res.status(200).json(flip)
+})
+
+app.get('/app/flip/call/tails', (req, res) => {
+    var flip = flipACoin('tails')
+    res.status(200).json(flip)
 })
 
 app.use(function(req, res) {
